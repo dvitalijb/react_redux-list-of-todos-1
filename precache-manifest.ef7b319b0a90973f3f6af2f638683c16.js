cce9e99fb0f4ed56ddca095b7501595e98edcb01
vitalii <dumavb777@gmail.com>
2019-07-09 16:33:35 +0300

    prod

---
+++ precache-manifest.ef7b319b0a90973f3f6af2f638683c16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd5ce37b539f950fb21d63b2b583d9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4fd8ab675e5e540e60b",
+    "url": "./static/css/main.7cbe4430.chunk.css"
+  },
+  {
+    "revision": "c6eaedc5f1835213227c",
+    "url": "./static/js/2.1163e828.chunk.js"
+  },
+  {
+    "revision": "e4fd8ab675e5e540e60b",
+    "url": "./static/js/main.aa96fc26.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
